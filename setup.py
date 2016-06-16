faaf0f878577898c1d03cfdd1a690f2f9249e53e
Nir Fuchs <nirf@gigaspaces.com>
2016-06-16 16:47:06 +0300

    bump cloudify-script-plugin version to 1.4.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-script-plugin',
-    version='1.4',
+    version='1.4.1',
     author='Gigaspaces',
     author_email='cloudify@gigaspaces.com',
     packages=['script_runner'],
